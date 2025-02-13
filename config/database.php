675b09fddbae5ea4c642a388bf8cff49a806d4a7
Deeppawandeep <pawandeepgei@gmail.com>
2025-02-13 11:19:20 -0700

    Configure DB & Redis variables

--- config/database.php
+++ config/database.php
@@ -144,7 +144,7 @@ return [
             'username' => env('AZURE_REDIS_PASSWORD'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
-            'database' => env('AZURE_REDIS_DATABASE', '1'),
+            'database' => env('AZURE_REDIS_DATABASE', '1'),  
         ],
 
     ],
